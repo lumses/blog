d193b7b1eb46581d614d1428973434b8be2dba77
Your Name <you@example.com>
2022-06-30 16:12:18 +0300

    Revert "Initial commit"
    This reverts commit db7f3c8e66b283072b7a4c9f8cee0eaf3f546fbb.

--- src/main/java/com/example/dudar/repo/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.dudar.repo;
-
-import com.example.dudar.models.Posts;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Posts, Long> {
-}
